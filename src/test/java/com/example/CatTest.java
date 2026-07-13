d09aa725ad67a5e4b42efd0503e55c4954576c64
gasilyas <il.gasin@yandex.ru>
2026-07-13 15:51:50 +0300

    первичные тесты

---
+++ src/test/java/com/example/CatTest.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void getSoundReturnsMeow() {
+        Cat cat = new Cat(feline);
+        assertEquals("Мяу", cat.getSound());
+    }
+
+    @Test
+    public void getFoodReturnsMeatSources() throws Exception {
+        Cat cat = new Cat(feline);
+        Mockito.when(feline.eatMeat().thenReturn(List.of("Животные", "Птицы", "Рыба"));
+
+        List<String> meatSources = List.of("Животные", "Птицы", "Рыба");
+        assertEquals(meatSources, cat.getFood());
+    }
+}
