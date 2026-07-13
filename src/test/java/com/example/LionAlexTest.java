b7e4605db650d89aaa4f3ff6b686e1bb0460fc38
gasilyas <il.gasin@yandex.ru>
2026-07-13 17:17:06 +0300

    финализация тестов

--- src/test/java/com/example/LionAlexTest.java
+++ src/test/java/com/example/LionAlexTest.java
@@ -1,4 +1,36 @@
 package com.example;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+import java.util.List;
+
 public class LionAlexTest {
+
+    @Test
+    public void getFriendsReturnsCorrectList() throws Exception {
+        // Создаем мок зависимости Feline для передачи в конструктор
+        Feline feline = Mockito.mock(Feline.class);
+        LionAlex alex = new LionAlex(feline);
+
+        List<String> expectedFriends = List.of("Марти", "Мелман", "Глория");
+
+        Assert.assertEquals(expectedFriends, alex.getFriends());
+    }
+
+    @Test
+    public void getPlaceOfLivingReturnsNewYorkZoo() throws Exception {
+        Feline feline = Mockito.mock(Feline.class);
+        LionAlex alex = new LionAlex(feline);
+
+        Assert.assertEquals("Нью-Йоркский зоопарк", alex.getPlaceOfLiving());
+    }
+
+    @Test
+    public void getKittensReturnsZero() throws Exception {
+        Feline feline = Mockito.mock(Feline.class);
+        LionAlex alex = new LionAlex(feline);
+
+        Assert.assertEquals(0, alex.getKittens());
+    }
 }
--- src/test/java/com/example/LionParametrizedTest.java
+++ src/test/java/com/example/LionParametrizedTest.java
@@ -1,4 +1,38 @@
 package com.example;
 
-public class LionParamtrizedTest {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mockito;
+import java.util.Arrays;
+import java.util.Collection;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class LionParametrizedTest {
+
+    private final String sex;
+    private final Boolean expectedHasMane;
+
+    public LionParametrizedTest(String sex, Boolean expectedHasMane) {
+        this.sex = sex;
+        this.expectedHasMane = expectedHasMane;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> inputData() {
+        return Arrays.asList(new Object[][]{
+                {"Самец", true},
+                {"Самка", false}
+        });
+    }
+
+    @Test
+    public void doesHaveManeReturnsCorrectValue() throws Exception {
+        Feline feline = Mockito.mock(Feline.class);
+        Lion lion = new Lion(feline, sex);
+
+        assertEquals(expectedHasMane, lion.doesHaveMane());
+    }
+
 }
